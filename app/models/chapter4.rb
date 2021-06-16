4d0058c002f0e85106166586319443970d5e9224
hapi190601 <gc1206ro@gmail.com>
2021-06-16 04:29:26 +0000

    Finish4-1~3

--- app/models/chapter4.rb
+++ app/models/chapter4.rb
@@ -1,2 +1,61 @@
-class Chapter4 < ApplicationRecord
+# class Chapter4 < ApplicationRecord
+# ４章配列の繰り返し処理
+
+puts [].class
+
+# 配列の中に配列を入れることも可能
+arraies = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+puts arraies
+puts arraies[2]
+
+# 配列の要素削除
+a = [1, 2, 3]
+a.delete_at(0)
+puts a
+
+# 値の更新・追加
+b = [1, 2, 3]
+
+b[0] = 50
+puts b
+
+# インデックス番号を指定して追加
+b = [1, 2, 3]
+b[3] = 4
+puts b
+
+# 配列の最後に要素を追加
+b = []
+b << 1
+b << 10
+b << 100
+puts b
+
+# ブロック(下記each文のdo~endまでを意味する)
+numbers = [1 ,2, 3, 4, 5, 6]
+sum = 0
+
+numbers.each do |n|
+  sum += n
 end
+
+puts sum
+
+# 配列の要素削除
+# delete, delete_at, delete_ifの違い
+# delete()は引数の値と完全一致した値を削除
+# delete_at()は引数の値をインデックス番号として配列を参照し、該当する要素を削除する
+# delete_ifはdo~end(ブロック)を使い、様々な条件を指定可能
+
+numbers = [1 ,2, 3, 4, 5, 6]
+sum = 0
+
+# do~endは{ }で書き換えることも可能(１行でまとめたいときなどに使うらしい)
+numbers.each { |n|
+  sum += n
+}
+
+puts sum
+
+
+# end
