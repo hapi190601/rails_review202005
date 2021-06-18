7a4198deb0b8966a56ff171604c66d51fc979658
hapi190601 <gc1206ro@gmail.com>
2021-06-18 10:14:25 +0000

    RenameAppName

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsReview202005
+module RailsReview202106
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
