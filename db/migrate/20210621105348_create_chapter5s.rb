1f2d6ccb391ba1ed79a0133fb73bd5551f51667e
hapi190601 <gc1206ro@gmail.com>
2021-06-21 10:54:05 +0000

    ５章雛形

---
+++ db/migrate/20210621105348_create_chapter5s.rb
@@ -0,0 +1,8 @@
+class CreateChapter5s < ActiveRecord::Migration[5.2]
+  def change
+    create_table :chapter5s do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/chapter5_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Chapter5Test < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
